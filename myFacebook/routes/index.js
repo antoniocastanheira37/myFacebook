7b97d701b10e0e2e5d8cd35fffe7e0cc7a00374a
SuperMarioBronze <healerequoin@hotmail.com>
2019-01-20 17:44:18 +0000

    package

--- myFacebook/routes/index.js
+++ myFacebook/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/mainPage', function(req, res, next) {
+  res.render('mainPage', { title: 'Express' });
+});
+
 module.exports = router;
